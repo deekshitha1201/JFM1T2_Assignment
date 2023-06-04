7459b0152ae4f4228ec7e420f13b26c901979acc
deekshitha1201 <132832721+deekshitha1201@users.noreply.github.com>
2023-06-04 17:40:45 +0530

    Add files via upload

---
+++ Operations.java
@@ -0,0 +1,53 @@
+/*  JFM1T2_Assignment1:
+
+     Write a program to perform the below actions and print the result.
+     1. Difference of 2 numbers (30 and 10)
+     2. Product of 2 numbers (45 and 2)
+     3. Division of 2 numbers (600 and 10)
+     4. Increment and Decrement the number (15)
+     5. Remainder of 2 numbers (14 and 5)
+     Prompt the user input from the terminal.
+*/
+
+//import statements for java program to read inputs using Scanner class
+import java.util.Scanner;
+
+public class Operations {
+  public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter a value");
+    int a=sc.nextInt();
+    System.out.println("enter b value");
+    int b=sc.nextInt();
+    int c=a-b;
+    System.out.println(c);
+    int d=a*b;
+    System.out.println(d);
+    int e=a%b;
+    System.out.println(e);
+    int f=a++;
+    System.out.println(f);
+    int g=a--;
+    System.out.println(g);
+    int h=a/b;
+    System.out.println(h);
+    
+  }
+}
+//main method
+
+/*
+  Use the scanner class to provide input at execution time using scanner object
+  Scanner sc=new Scanner(System.in);
+*/
+
+/*
+  Take input from user
+  System.out.println("Enter first number: ");
+  int a=sc.nextInt();
+*/
+
+//calculate difference,product,division,increment and remainder 
+
+//print appropriate result for all operations
+
---
+++ SumWithoutPlus.java
@@ -0,0 +1,52 @@
+/*  JFM1T2_Assignment5:
+
+    Write a program to print the sum of two numbers without using + operator.
+    Prompt the user input from the terminal.
+    
+    Sample Input:
+    Enter first number: 
+    22
+    Enter second number: 
+    50
+    
+    Expected Output:
+    The sum of two numbers is: 72
+*/
+
+//import statements for java program to read inputs using Scanner class
+import java.util.Scanner;
+
+public class SumWithoutPlus {
+
+//main method
+public static void main(String[] args){
+  Scanner sc=new Scanner(System.in);
+  System.out.println("enter a value");
+  int a=sc.nextInt();
+  System.out.println("enter b value");
+  int b=sc.nextInt();
+  System.out.println(add( a, b));
+}
+  public static int add(int a,int b){
+  return(a+b);
+  
+    
+  }
+}
+/*
+  Use the scanner class to provide input at execution time using scanner object
+  Scanner sc=new Scanner(System.in);
+*/
+
+/*
+  Take input from user
+  System.out.println("Enter first number: ");
+  int a=sc.nextInt();
+*/
+
+//check if the second number is greater than zero then increase first number and decrease second number
+
+//check if the second number is less than zero then decrease first number and increase second number
+
+//print result
+
